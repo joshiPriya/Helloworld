c33c297daef4daab3195f316b00596c919b87002
joshiPriya <43839921+joshiPriya@users.noreply.github.com>
2019-12-21 11:55:56 +0530

    commit
    commit

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World to all!" );
+        System.out.println( "Hello World to all of us!" );
 
        }
 }
